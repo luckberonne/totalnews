2f7be4f08bae73cf0d50589dc3db593bbf1f26be
Lucas Beronne <luckberonne@gmail.com>
2024-03-18 13:06:02 -0300

    modificaciones cron

--- src/pages/api/cron.ts
+++ src/pages/api/cron.ts
@@ -7,27 +7,44 @@ import { NextApiRequest, NextApiResponse } from "next";
 const genAI = new GoogleGenerativeAI("AIzaSyDxUtirxFw02eGbtD6I1gd_lGnp98d_7pI");
 
 
+function parsearTexto(texto: string) {
+  const secciones = texto.split("**Título:**");
+  const titulo = secciones[1].split("**Subtítulo:**")[0].trim();
+  const subtitulo = secciones[1].split("**Subtítulo:**")[1].split("**Lead:**")[0].trim();
+  const lead = secciones[1].split("**Lead:**")[1].split("**Cuerpo:**")[0].trim();
+  const cuerpo = secciones[1].split("**Cuerpo:**")[1].split("**Extra:**")[0].trim();
+  const extra = secciones[1].split("**Extra:**")[1].trim();
+
+  return {
+    titulo,
+    subtitulo,
+    lead,
+    cuerpo,
+    extra
+  };
+}
+
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   try {
     // For text-only input, use the gemini-pro model
     const model = genAI.getGenerativeModel({ model: "gemini-pro" });
 
     const prompt = `
-    As an imaginative journalist for a quirky and fictional newspaper, your forte lies in creating hilarious and entertaining fake news stories that captivate readers with their creativity and humor. Your task is to craft a funny and absurd news piece for our newspaper. Here are the details you need to include:
-    - *Headline*: [Blank](this in spanish)
-    - *Subtitle*: [Blank](this in spanish)
-    - *Lead*: [Blank](this in spanish)
-    - *Body*: [Blank](this in spanish)
-    - *Extra*: [Blank](this in spanish)
-    - *Image Prompt*: [Blank](this in english)
+    (in spanish) As an imaginative journalist for a quirky and fictional newspaper, your forte lies in creating hilarious and entertaining fake news stories that captivate readers with their creativity and humor. Your task is to craft a funny and absurd news piece for our newspaper. Here are the details you need to include:
+    - *Headline*: [Blank]
+    - *Subtitle*: [Blank]
+    - *Lead*: [Blank]
+    - *Body*: [Blank]
+    - *Extra*: [Blank]
+    - *Image Prompt*: [Blank]
     Your goal is to maintain a consistent structure for the news article with a touch of humor in every section. Ensure the headline is attention-grabbing, the lead captures the essence of the story, and the body elaborates on the details in a comical and engaging manner. Add a quirky twist at the end to leave the readers amused. Remember to keep the tone light-hearted and the content witty throughout the article.`;
 
     const result = await model.generateContent(prompt);
     const response = await result.response;
     const text = response.text();
     console.log(text);
-    // const noticia = parsearTexto(text);
-    // await PostNoticias(noticia)
+    const noticia = parsearTexto(text);
+    await PostNoticias(noticia)
     res.send({message: "All message sent successfully."})
   }
   catch (error) {
