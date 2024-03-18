a733e4ae93229961b67ff52fc70fd69952f5f7dc
Lucas Beronne <luckberonne@gmail.com>
2024-03-18 15:49:29 -0300

    redeploy

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -11,7 +11,7 @@ export default function Admin() {
             console.error("Error executing function run():", error);
         }
     };
-
+ 
     return (
         <div>
             <Button onClick={handleRun}>Run Function</Button>
