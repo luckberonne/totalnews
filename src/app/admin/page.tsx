bb5311db719060d41487d41afc83b56ffeb743ea
Lucas Beronne <luckberonne@gmail.com>
2024-03-18 16:05:58 -0300

    nombre boton

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,11 +1,11 @@
 'use client'
-// import run from "@/pages/api/cron";
+import run from "@/pages/api/cron";
 import { Button } from "@nextui-org/react";
 
 export default function Admin() {
     const handleRun = async () => {
         try {
-            // await run();
+            await run();
             console.log("Function run() executed successfully.");
         } catch (error) {
             console.error("Error executing function run():", error);
@@ -14,7 +14,7 @@ export default function Admin() {
  
     return (
         <div>
-            <Button onClick={handleRun}>Run Function</Button>
+            <Button onClick={handleRun}>Generar Noticia</Button>
         </div>
     );
 }
